a58cc4957a3d7c65aaec012c93ad53149873e9ae
Minh Nguyen <ro.gtvt@gmail.com>
2014-11-22 10:48:40 +0700

    Update SignActivity.java

--- src/com/rocrazy/signview/SignActivity.java
+++ src/com/rocrazy/signview/SignActivity.java
@@ -3,6 +3,7 @@ package com.rocrazy.signview;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 import android.os.Bundle;
 
